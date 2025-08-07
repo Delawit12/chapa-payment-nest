77e7b2a480fd1dec88c46dfa44c4efda9eeeaed9
Delawit12 <131721430+Delawit12@users.noreply.github.com>
2025-08-07 10:21:43 +0300

    chore: environment setup for the logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChapaService } from './chapa/chapa.service';
+import { ChapaConfigService } from './chapa-config/chapa-config.service';
+import { PaymentsController } from './payments/payments.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PaymentsController],
+  providers: [AppService, ChapaService, ChapaConfigService],
 })
 export class AppModule {}
